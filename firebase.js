abd4129e3657f0c002894a53d458ebb9a6065d8d
Alan Hurtarte <alan220193@hotmail.com>
2019-11-27 07:28:05 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import app from 'firebase/app';
 
 var firebaseConfig = {
     apiKey: "",
-    authDomain: "test-1-fbadc.firebaseapp.com",
-    databaseURL: "https://test-1-fbadc.firebaseio.com",
-    projectId: "test-1-fbadc",
-    storageBucket: "test-1-fbadc.appspot.com",
-    messagingSenderId: "221557899841",
-    appId: "1:221557899841:web:276c40ec1254f4c879bcc0",
-    measurementId: "G-T319603820"
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 
