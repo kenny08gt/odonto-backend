9fc28892488ae72ce0648523bc51a5b0afa0731a
Alan Hurtarte <alan@mypostcard.com>
2019-11-30 10:36:30 -0600

    Added migration files

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require("Sequelize");
+const Sequelize = require("sequelize");
 
 var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
