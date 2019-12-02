9a082441e7a0fc1aabf17c2ead950e11a9bb1931
Brayan Yaquián <bragonya@gmail.com>
2019-12-02 14:49:13 -0600

    change port

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/*', function (req, res) {
 });
 //app.listen(9000);
 var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(7043);
+httpsServer.listen(443);
 const server = http.createServer(app);
 const io = socketIo(server);
 let users = [];
